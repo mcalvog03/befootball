e7a112533975be18dbdd1dc64d3adb06d94c285f
Miguel <mcalvog03@educantabria.es>
2025-03-07 17:46:43 +0100

    Generar proyecto con dependencias

---
+++ Befootball/Befootball/src/main/java/com/mycompany/befootball/Befootball.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.befootball;
+
+public class Befootball {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
